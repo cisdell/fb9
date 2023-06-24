c1c68c63b9b9db457c510bdc01ad8dc8facef4ff
Andrew Cho <cisdell@gmail.com>
2023-06-23 23:32:48 -0400

    added firebase and firestore ver.9

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBs8WX2Q27fxn-OREIGHFj1vndKyoXWMKQ",
+  authDomain: "readinglistapp-37762.firebaseapp.com",
+  projectId: "readinglistapp-37762",
+  storageBucket: "readinglistapp-37762.appspot.com",
+  messagingSenderId: "618113375038",
+  appId: "1:618113375038:web:3a9aa7bb7ffae691c02a6f",
+};
+
+//init firebase
+initializeApp(firebaseConfig);
+
+// init firestore
+const db = getFirestore();
+
+export { db };
