a6472d0055eafd091bfd5489b15d23b2bf5d7c65
Andrew Cho <cisdell@gmail.com>
2023-06-25 19:46:37 -0400

    added signup, logout and signin

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBs8WX2Q27fxn-OREIGHFj1vndKyoXWMKQ",
@@ -16,4 +17,8 @@ initializeApp(firebaseConfig);
 // init firestore
 const db = getFirestore();
 
-export { db };
+
+//init firebase auth
+const auth = getAuth()
+
+export { db, auth };
